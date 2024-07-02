75eb86afb69193bb441f38b96d135b7d774cf5c9
minuk302 <minuk302@naver.com>
2024-07-02 14:52:33 +0900

    adds secret file for configs

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ To set up the Conda environment necessary for this project, run the following co
 
 ```bash
 conda env create -f environment.yml
+```
+
+## Setting Up the Config Values
+Change the _secret.ini file name to secret.ini and fill out each config value.
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
+import configparser
+
+config = configparser.ConfigParser()
+config.read('secret.ini')
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+account_sid = config['DEFAULT']["TWILIO_ACCOUNT_SID"]
+auth_token = config['DEFAULT']["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
     url="http://demo.twilio.com/docs/voice.xml",
     to="+15558675310",
-    from_="+15017122661",
+    from_=config['DEFAULT']["TWILIO_PHONE_NUMBER"],
 )
 
 print(call.sid)
